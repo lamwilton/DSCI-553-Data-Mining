21680437278608de5fb1f1e32b1e7b069c8f5713
lamwilton <lamwiltonusa@gmail.com>
2020-05-25 19:01:12 -0700

    Task 1 done

--- HW1/task1.py
+++ HW1/task1.py
@@ -1,4 +1,4 @@
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext
 import json
 import sys
 
@@ -16,23 +16,68 @@ def part_b(lines, y):
     return year_count
 
 
-def part_c(lines):
+def part_c_d(lines, m):
+    # Part C
     users = lines.map(lambda s: (json.loads(s)['user_id'], 1))
     users_group = users.groupByKey()
     users_count = users_group.count()
+
+    # Part D
+    top_users = users.reduceByKey(lambda a, b: a + b)
+    top_users_col = top_users.collect()
+    top_users_col = sorted(top_users_col, key=lambda x: -x[1])
+
+    top_m = top_users_col[0:m]
+    top_m_list = [[str(elem[0]), elem[1]] for elem in top_m]  # Convert to list format from tuple
     print("The number of unique users is : " + str(users_count))
-    return users_count
+    print("Top " + str(m) + " users: " + str(top_m_list[0:10]))
+    return users_count, top_m_list
+
+
+def part_e(lines, n):
+    stopwords_file = sys.argv[3]
+    with open(stopwords_file) as file:
+        stopwords = file.read().splitlines()
+
+    # process each input line by first loading, then remove punctuation/to lower case, finally remove stopwords
+    reviews = lines.map(lambda line: json.loads(line)['text'])\
+        .map(lambda line: line.translate({ord(i): None for i in '([,.!?:;])'}).lower())\
+        .map(lambda line: " ".join([word for word in line.split() if word not in stopwords]))
+
+    # Regular spark Word count https://spark.apache.org/examples.html
+    counts = reviews.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
+    count_result = counts.collect()
+
+    count_result = sorted(count_result, key=lambda x: -x[1])
+    top_n = count_result[0:n]
+    top_n_list = [str(elem[0]) for elem in top_n]  # Convert to list format from tuple
+    print("Top " + str(n) + "list is : " + str(top_n_list))
+    return top_n_list
 
 
 if __name__ == '__main__':
-    conf = SparkConf().setAppName("task1").setMaster("local[6]")
-    sc = SparkContext(conf=conf)
+    """
+    Run command: spark-submit task1.py $ASNLIB/public_data/review.json (output) $ASNLIB/public_data/stopwords 2011 10 10 
+    sys.argv: 1 = input_file 2 = output_file, 3 = stopwords, 4 = y, 5 = m, 6 = n
+    """
+    sc = SparkContext(master="local[6]", appName="task1")
+    sc.setLogLevel("WARN")
     lines = sc.textFile(sys.argv[1])
     answer = dict()
     answer['A'] = part_a(lines)
     y = str(sys.argv[4])
     answer['B'] = part_b(lines, y=y)
-    answer['C'] = part_c(lines)
+    answer['C'], answer['D'] = part_c_d(lines, m=int(sys.argv[5]))
+    answer['E'] = part_e(lines, n=int(sys.argv[6]))
+
+    # Write answers to file
+    # TODO: Make sure single quotes are ok
+    file = open(sys.argv[2], "w")
+    file.write(str(answer))
+    file.close()
+
     print("Final answers:")
-    print(answer)
+    print(str(answer))
     exit()
