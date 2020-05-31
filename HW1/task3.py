82f92dd99da31f25870ce54dca1b2d5dd6e93be3
lamwilton <lamwiltonusa@gmail.com>
2020-05-30 21:33:17 -0700

    task3improve

--- HW1/task3.py
+++ HW1/task3.py
@@ -3,6 +3,7 @@ import json
 import sys
 import time
 
+
 if __name__ == '__main__':
     time1 = time.time()
     sc = SparkContext(master="local[*]", appName="task3")
@@ -19,10 +20,10 @@ if __name__ == '__main__':
 
     # After mapping, partition is lost because key is changed, so should partition again to increase speed
     if part_type == "customized":
-        review_lines2 = review_lines2.repartition(part)
+        review_lines2 = review_lines2.partitionBy(part, lambda x: ord(x[0]))
 
     # Count and filter for more than n reviews
-    review_lines4 = review_lines2.reduceByKey(lambda x, y: x + y) \
+    review_lines4 = review_lines2.reduce(lambda x, y: x + y) \
         .filter(lambda x: x[1] > n)
 
     answer = dict()
