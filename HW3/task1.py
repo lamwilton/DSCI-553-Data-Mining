604be52ce17773ea9d17af1671ec52e605d214e9
lamwilton <lamwiltonusa@gmail.com>
2020-06-10 02:06:14 -0700

    comment

--- HW3/task1.py
+++ HW3/task1.py
@@ -6,11 +6,11 @@ from collections import defaultdict
 import random
 
 
-def char_table(input_file):
+def char_table():
     """
     Read file and convert business id to numbers
-    :param input_file:
     :return: characteristic table(rows = users, each row is set of businesses), list of businesses (from number to string)
+    eg [(userid1, {12, 2, 4, 23, 42, 25}), ...] and [businessid1, businessid2, businessid3, ...]
     """
     def tablehelper(x):
         """
@@ -44,7 +44,6 @@ def char_table(input_file):
     for i in range(len(businesslist)):
         businessinv[businesslist[i]] = i
     businessinv1 = sc.broadcast(businessinv)
-    num_business = len(businessinv)
 
     # Generate characteristic table (rows = users, each row is set of businesses)
     table = users1.map(lambda x: tablehelper(x))
@@ -64,6 +63,7 @@ def minhash(table, a, b, num_business):
     :param a:
     :param b:
     :return: Minhash of one hash function
+    eg [749, 724, 194, 11, 103, 115, 216, 955, 192, 322, 105, 704, 32, ...]
     """
     table = table.value
     m = len(table)
@@ -79,7 +79,8 @@ def minhash(table, a, b, num_business):
 def hash_func_generate(num_func):
     """
     Generate hash functions a and b
-    :return:
+    :return: list of a and b pairs
+    eg [[983, 294], [1777, 208], [557, 236], ...]
     """
     primes = [547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987]
     result = []
@@ -110,6 +111,7 @@ def lsh_signature(minhashes):
 def business_table():
     """
     Generate business table for Jaccard (Rows = business)
+    eg [('businessid1', {'userid1', ...}), ...]
     :return: Dictionary of businesses
     """
     busi_table = lines.filter(lambda line: len(line) != 0) \
@@ -159,7 +161,7 @@ if __name__ == '__main__':
 
     # Read file and convert business id to numbers
     lines = sc.textFile(input_file).distinct().persist()
-    table, businesslist = char_table(input_file)
+    table, businesslist = char_table()
     num_business = len(businesslist)
 
     # Doing the Minhashing
@@ -191,7 +193,7 @@ if __name__ == '__main__':
     # Jaccard and final results
     final_result = list(map(jaccard, result_lsh))
     hits = list(filter(lambda x: x[2] >= 0.05, final_result))
-    print("True positives: " + str(len(hits)))
+    print("All positives: " + str(len(final_result)) + " True positives (Need 47548): " + str(len(hits)) + " Accuracy: " + str(len(hits) / (len(final_result) + 0.001)))
 
     # Ending
     totaltime = time.time() - time1
