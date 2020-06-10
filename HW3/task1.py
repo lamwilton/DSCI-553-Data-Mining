0e22b431179363f430f5fd81dd01a38b2613a3b9
lamwilton <lamwiltonusa@gmail.com>
2020-06-09 22:37:58 -0700

    small change

--- HW3/task1.py
+++ HW3/task1.py
@@ -3,6 +3,7 @@ import sys
 import time
 import json
 from collections import defaultdict
+import random
 
 
 def char_table(input_file):
@@ -49,6 +50,7 @@ def char_table(input_file):
     table = users1.map(lambda x: tablehelper(x))
     table1 = table.values().collect()  # Remove user ids
 
+    # Ending
     businessinv1.destroy()
     totaltime = time.time() - time1
     print("Duration table: " + str(totaltime))
@@ -73,6 +75,7 @@ def minhash(table, a, b, num_business):
                 result[business_id] = hashvalue
     return result
 
+
 def hash_func_generate(num_func):
     """
     Generate hash functions a and b
@@ -80,8 +83,11 @@ def hash_func_generate(num_func):
     """
     primes = [31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997,1009,1013,1019,1021,1031,1033,1039,1049,1051,1061,1063,1069,1087,1091,1093,1097,1103,1109,1117,1123,1129,1151,1153,1163,1171,1181,1187,1193,1201,1213,1217,1223]
     result = []
+    random.shuffle(primes)
+    b = list(range(1, num_func + 1))
+    random.shuffle(b)
     for i in range(0, num_func):
-        result.append([primes[i], i+1])
+        result.append([primes[i], b[i]])
     return result
 
 
@@ -91,6 +97,7 @@ def lsh_signature(minhashes):
     :param minhashes: 2d list of minhashes
     :return: Set of Candidate pairs
     """
+    # TODO: Use defaultdict to increase efficiency
     result = set()
     num_business = len(minhashes[0])
     for i in range(num_business):
@@ -137,6 +144,8 @@ def jaccard(pair):
 
 
 if __name__ == '__main__':
+
+    # Initializing
     time1 = time.time()
     conf = SparkConf()
     conf.set("spark.driver.memory", "4g")
@@ -176,8 +185,10 @@ if __name__ == '__main__':
     totaltime = time.time() - time1
     print("Duration LSH: " + str(totaltime))
 
+    # Making business table (inverse char table, each row = business)
     busi_dict = business_table()
 
+    # Jaccard and final results
     final_result = list(map(jaccard, result_lsh))
     hits = list(filter(lambda x: x[2] >= 0.05, final_result))
     print("True positives: " + str(len(hits)))
--- HW3/task1tester.py
+++ HW3/task1tester.py
@@ -19,6 +19,27 @@ def minhash(table, a, b, num_business):
     return result
 
 
+def jaccard(a, b):
+    """
+    Find jaccard of a pair of business
+    :param pair: business num (int)
+    :return: tuple of business_id and their Jaccard similarity
+    """
+    # Find back the business ids
+    #business_a = businesslist[pair[0]]
+    #business_b = businesslist[pair[1]]
+
+    # Look up busi dict for the sets
+    #a = busi_dict[business_a]
+    #b = busi_dict[business_b]
+    union = len(a.union(b))
+    if union == 0:
+        similarity = 0
+    else:
+        similarity = len(a.intersection(b)) / union
+    return tuple((a, b, similarity))
+
+
 if __name__ == '__main__':
     print("Testing minhash======================================================")
     print(str(minhash([{0,3},{2},{1},{0,2,3},{2}], a=1, b=1, num_business=4)) + " Expected [1, 3, 0, 1]")
@@ -26,6 +47,12 @@ if __name__ == '__main__':
     print(str(minhash([{0},{0,1},{0,1},{1},{0,1},{0},{1}], a=1, b=1, num_business=2)) + " Expected [1, 0]")
     print(str(minhash([{0},{0,1},{0,1},{1},{0,1},{0},{1}], a=1, b=4, num_business=2)) + " Expected [1, 0]")
 
+    print("Testing hash function generator======================================================")
+    print(task1.hash_func_generate(190))
+
     print("Testing signature======================================================")
     print(task1.lsh_signature([[1, 0, 2, 4, 5, 3, 2, 1, 1], [2, 0, 2, 6, 1, 5, 4, 8, 2]]))
     print(task1.lsh_signature([[1, 0, 2, 4, 4, 3, 2, 1, 1], [2, 0, 2, 6, 6, 5, 4, 8, 2]]))
+
+    print("Testing Jaccard======================================================")
+    print(jaccard({1, 2}, {1, 2, 4}))
