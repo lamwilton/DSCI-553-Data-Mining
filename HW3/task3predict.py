3700ad3b47aedfe25d6741fd6abc5e96130f16e0
lamwilton <lamwiltonusa@gmail.com>
2020-06-17 17:22:52 -0700

    userbased predict real done

--- HW3/task3predict.py
+++ HW3/task3predict.py
@@ -139,6 +139,79 @@ def prediction_item_based(user, business):
     return result
 
 
+def initialize_user_based():
+    """
+    User based case, read test and model files and process them, also process reviews to 2d dictionary
+    :return: model_dict and test_pairs (rdd)
+    model_dict eg {0: [(5152, 0.3227486121839514), (7364, 1.0), ...], 7336: [(8764, 0.17270852553681604), (8064, 0.3400721382955765), ...], ...}
+    reviews_dict eg {0: {8761: 5.0, 56: 3.0, ...}, 24276: {4: 5.0, 5173: 2.0, 9630: 1.0, 2285: 5.0, 7897: 5.0, 6750: 4.0, 9497: 1.0}, ...}
+    test_pairs (userid, businessid) eg (13465, 8849), (21851, 958), (15944, 3831), (9517, 5264), (12273, 5911), ...
+    """
+    # Read test file
+    # eg (13465, 8849), (21851, 958), (15944, 3831), (9517, 5264), (12273, 5911), ...
+    test_pairs = test_lines.filter(lambda line: len(line) != 0) \
+        .map(lambda s: (json.loads(s)['user_id'], json.loads(s)['business_id'])) \
+        .filter(lambda x: x[0] is not None and x[1] is not None and x[0] != "" and x[1] != "") \
+        .map(lambda x: (users_dict[x[0]], businesses_dict[x[1]]))
+
+    # Read Model file
+    model_pairs = model_lines.filter(lambda line: len(line) != 0) \
+        .map(lambda line: (json.loads(line))) \
+        .map(lambda x: (x['u1'], x['u2'], x['sim'])) \
+        .filter(lambda x: x[0] is not None and x[1] is not None and x[2] is not None and x[0] != "" and x[1] != "" and x[2] != "") \
+        .map(lambda x: (users_dict[x[0]], (users_dict[x[1]], x[2])))
+
+    # Switch u1 and u2
+    model_pairs_dup = model_pairs.map(lambda x: (x[1][0], (x[0], x[1][1])))
+
+    # Concatanate the pairs so u1/u2 and u2/u1, group by key and Output model file as dictionary
+    # eg {6012: {24061: 0.7938841860374448, 15302: 0.30151134457776363, ...,}, 13724: {15996: 1.0, 24759: 0.5, ...}, ...}
+    model_dict = model_pairs.union(model_pairs_dup)\
+        .groupByKey()\
+        .map(lambda x: (x[0], dict(x[1].data)))\
+        .collectAsMap()
+
+    # Convert reviews rdd to a 2d dictionary (business then user)
+    # eg {0: {24267: 1.0, 5670: 3.0, 15085: 2.0, ...}, ...}
+    reviews_dict = reviews.map(lambda x: (x[1], (x[0], x[2])))\
+        .groupByKey()\
+        .map(lambda x: (x[0], dict(x[1].data)))\
+        .collectAsMap()
+
+    # Filter out businesses/users not in model
+    businesses = set(reviews_dict.keys())
+    users = set(model_dict.keys())
+    test_pairs = test_pairs.filter(lambda x: x[0] in users and x[1] in businesses)
+    return model_dict, reviews_dict, test_pairs
+
+
+def prediction_user_based(user, business):
+    """
+    User-based prediction formula
+    :param user: Testing user
+    :param business: Testing business
+    :return: Predicted rating
+    """
+    K = 3  # Number of nearest neighbours to use
+    sim_user = model_dict[user]  # Get all similar businesses from model of business of interest
+    business_reviews = reviews_dict[business]  # Get reviews of user of interest
+    neighbours = set(sim_user.keys()).intersection(set(business_reviews.keys()))  # Get similar businesses which is rated by that user of interest
+
+    # Get a list of ratings and weights sorted by weights
+    # eg [(0.692833855070933, 5.0), (0.6305538695530374, 5.0), (0.5706052915642571, 2.0), ...]
+    ratings_list = sorted([(sim_user[i], business_reviews[i]) for i in neighbours], key=lambda x: -x[0])
+    ratings_sublist = ratings_list[0:K]
+    numerator = sum([x * y for x, y in ratings_sublist])
+    denominator = sum([x for x, y in ratings_sublist])
+
+    # If no corated users at all, return average score of user. If user not in model, return the "UNK" default score
+    if numerator == 0 or denominator == 0:
+        return user_avg.get(user, user_avg.get("UNK"))
+    else:
+        result = numerator / denominator
+    return result
+
+
 if __name__ == '__main__':
 
     # ========================================== Initializing ==========================================
@@ -171,6 +244,8 @@ if __name__ == '__main__':
     model_lines = sc.textFile(model_file).distinct()
     if cf_type == "item_based":
         model_dict, reviews_dict, test_pairs = initialize_item_based()
+    else:
+        model_dict, reviews_dict, test_pairs = initialize_user_based()
     totaltime = time.time() - time1
     print("Duration read test/model: " + str(totaltime))
 
@@ -178,6 +253,8 @@ if __name__ == '__main__':
     # Item based eg ((('QtMgqKY_GF3XkOpXonaExA', 'IJUCRd5v-XLkcGrKjb8IfA'), 4.096186807416058), (('nwvnNIixvyYTg4JS8g3Xgg', 'WQyGqfFKd-baBTVfZWzeTw'), 3.097635508434237)
     if cf_type == "item_based":
         result = test_pairs.map(lambda x: ((users_inv[x[0]], businesses_inv[x[1]]), prediction_item_based(x[0], x[1]))).collect()
+    else:
+        result = test_pairs.map(lambda x: ((users_inv[x[0]], businesses_inv[x[1]]), prediction_user_based(x[0], x[1]))).collect()
     totaltime = time.time() - time1
     print("Duration predict: " + str(totaltime))
 
