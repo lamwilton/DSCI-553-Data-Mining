1cf0c2557c2f06d3c1e39f36ac2d98a0ff2253ed
lamwilton <lamwiltonusa@gmail.com>
2020-06-19 00:43:45 -0700

    task1 progress

--- HW4/task1.py
+++ HW4/task1.py
@@ -10,7 +10,6 @@ from pyspark.sql import SparkSession, Row
 from pyspark.sql import Row
 
 
-
 def corated_helper(user_reviews_dict, a, b):
     """
     Check if user pair has more than 7 corated businesses
@@ -29,7 +28,7 @@ def graph_construct():
     Reviews count = 38648
     Business count (before filtering) = 9947
     Users count (before filtering) = 3374
-    Users count (after filtering) = 3374
+    Users count (after filtering, vertices) = 222
     Edges count = 498
     :return: Users (Vertices) and Candidate pairs (Edges)
     """
@@ -73,15 +72,18 @@ def graph_construct():
 
     # Filter only corated businesses >= 7
     # eg [(0, 10), (0, 14), (0, 16), (0, 20), (0, 28), (0, 30), (0, 32), ...]
-    candidate_pairs = all_pairs.filter(lambda x: corated_helper(user_reviews_dict, x[0], x[1])) \
-        .collect()
-    return users, candidate_pairs, users_dict, users_inv
+    candidate_pairs_pre = all_pairs.filter(lambda x: corated_helper(user_reviews_dict, x[0], x[1])).persist()
+    candidate_pairs_pre_2 = candidate_pairs_pre.map(lambda x:(x[1], x[0]))
+    candidate_pairs = candidate_pairs_pre.union(candidate_pairs_pre_2).collect()
+    candidate_users = candidate_pairs_pre.flatMap(lambda x: [x[0], x[1]]).distinct().collect()
+    return candidate_users, candidate_pairs, users_dict, users_inv
 
 
 if __name__ == '__main__':
 
     # ========================================== Initializing ==========================================
     time1 = time.time()
+    os.environ["PYSPARK_SUBMIT_ARGS"] = ("--packages graphframes:graphframes:0.6.0-spark2.3-s_2.11  pyspark-shell")
     conf = SparkConf()
     conf.set("spark.driver.memory", "4g")
     conf.set("spark.executor.memory", "4g")
@@ -97,11 +99,37 @@ if __name__ == '__main__':
     community_output_file_path = sys.argv[3]
 
     # ========================================== Graph Construction ==========================================
-    users, candidate_pairs, users_dict, users_inv = graph_construct()
+    candidate_users, candidate_pairs, users_dict, users_inv = graph_construct()
     row = Row("id")
-    vertices = sc.parallelize(users).map(row).toDF()
-    edges = sc.parallelize(candidate_pairs).toDF(["s", "t"])
+    vertices = sc.parallelize(candidate_users).map(row).toDF()
+    edges = sc.parallelize(candidate_pairs).toDF(["src", "dst"])
     graph = GraphFrame(vertices, edges)
 
     totaltime = time.time() - time1
     print("Duration Graph Construction: " + str(totaltime))
+
+    # ========================================== LPA ==========================================
+    # Do LPA and return as dataframe
+    # eg (id, label) = (26, 1), (29, 0), ...
+    result = graph.labelPropagation(maxIter=5)
+
+    # Convert dataframe back to RDD, map with label as key and group by key. Also translate back to long userids
+    # Have to coalesce or else it gets stuck
+    pre_result = result.rdd.coalesce(2)\
+        .map(lambda x: (users_inv[x[1]], users_inv[x[0]]))\
+        .groupByKey()
+
+    # Drop keys and Sort lexigraphical order
+    final_result = pre_result.map(lambda x: sorted(x[1].data))\
+        .sortBy(lambda x: (len(x), x[0]))\
+        .collect()
+
+    # ======================================= Write results =======================================
+    with open(community_output_file_path, "w") as file:
+        for item in final_result:
+            line = str(item).strip("[]")
+            file.write(line)
+            file.write("\n")
+
+    totaltime = time.time() - time1
+    print("Duration: " + str(totaltime))
