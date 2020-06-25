78b090de055b5baaaa133557eaf10e2f132c5d95
lamwilton <lamwiltonusa@gmail.com>
2020-06-25 14:20:58 -0700

    hw5task2 deterministic hashes

--- HW5/task2tester.py
+++ HW5/task2tester.py
@@ -1,5 +1,13 @@
 import task2
+import random
+import sys
 
-print(task2.convert_str("olaslkas"))
-print(task2.count_zeros(1024))
-print(task2.reduce_helper(16, 103))
+
+#print(task2.convert_str("olaslkas"))
+#print(task2.count_zeros(1024))
+#print(task2.reduce_helper(16, 103))
+
+a = random.sample(range(1000000, sys.maxsize), 40)
+b = random.sample(range(1000000, sys.maxsize), 40)
+print(a)
+print(b)
