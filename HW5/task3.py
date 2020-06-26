e783a8986f93cbb7b44075acc5111642256ff089
lamwilton <lamwiltonusa@gmail.com>
2020-06-25 18:51:42 -0700

    hw5task3 corrected tagscount

--- HW5/task3.py
+++ HW5/task3.py
@@ -12,23 +12,24 @@ class MyStreamListener(tweepy.StreamListener):
         :param status:
         :return:
         """
-        global tweet_counter, tags_bin
+        global tweet_counter, tags_bin, tags_counter
         MAX_TAGS = 100
 
         # Reading a tweet
         tweet_tags = [i['text'] for i in status.entities['hashtags']]
         if len(tweet_tags) == 0:
-            return
+            return  # Ignore tagless tweets
         tweet_counter += 1
         # print("The number of tweets with tags from the beginning: " + str(tweet_counter))
 
         for tag in tweet_tags:
+            tags_counter += 1
             # For first 100 tags, directly save them
             if len(tags_bin) < MAX_TAGS:
                 tags_bin.append(tag)
             else:
                 # Decide to keep the tweet or not
-                keep = random.randint(0, tweet_counter - 1)
+                keep = random.randint(0, tags_counter - 1)
                 if keep <= MAX_TAGS:  # 100/n probability of keeping the new tweet (n = tweet_counter)
                     replacing = random.randint(0, MAX_TAGS - 1)  # Pick a random one to replace
                     tags_bin[replacing] = tag
@@ -84,6 +85,7 @@ if __name__ == '__main__':
     myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
     tweet_counter = 0
+    tags_counter = 0
     tags_bin = []
     open(output_file_name, "w")  # Create file if not exist
 
